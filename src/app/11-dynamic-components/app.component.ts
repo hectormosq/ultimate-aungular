96a5392cfd0495fa2518447d1c086c239a85f679
hectormosq <hectormosq@gmail.com>
2019-04-23 12:32:38 +0200

    Dynamic Components

---
+++ src/app/11-dynamic-components/app.component.ts
@@ -0,0 +1,31 @@
+import {
+  AfterContentInit,
+  Component,
+  ViewChild,
+  ViewContainerRef,
+  ComponentFactoryResolver
+} from "@angular/core";
+
+import { User } from "./auth-form/auth-form.interface";
+import { AuthFormComponent } from "./auth-form/auth-form.component";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "app.component.html"
+})
+export class AppComponent implements AfterContentInit {
+  @ViewChild("entry", { read: ViewContainerRef }) entry: ViewContainerRef;
+
+  constructor(private resolver: ComponentFactoryResolver) {}
+
+  ngAfterContentInit() {
+    const authFormFactory = this.resolver.resolveComponentFactory(
+      AuthFormComponent
+    );
+    const component = this.entry.createComponent(authFormFactory);
+  }
+
+  loginUser(user: User) {
+    console.log("Login", user);
+  }
+}
