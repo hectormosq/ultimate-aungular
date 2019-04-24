9fda7e8125e581db755bad1958694418ae4d5759
Mosquera Quiñonez <hjmosquera@eservicios.indra.es>
2019-04-24 15:51:37 +0200

    export as

---
+++ src/app/25-export-as/app.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component, ViewEncapsulation,
+} from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: "app.component.html",
+  styleUrls: ['./app.css']
+})
+export class AppComponent {
+}
---
+++ src/app/25-export-as/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { CreditCardDirective } from "./credit-card/credit-card.directive";
+import { TooltipDirective } from "./tooltip/tooltip.directive";
+
+@NgModule({
+  declarations: [AppComponent, CreditCardDirective, TooltipDirective],
+  imports: [BrowserModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
