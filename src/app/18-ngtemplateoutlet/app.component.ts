897bbc18334827bbb5d8c8debf63a36f073666b6
Mosquera Quiñonez <hjmosquera@eservicios.indra.es>
2019-04-24 10:11:28 +0200

    ng template outlet

---
+++ src/app/18-ngtemplateoutlet/app.component.ts
@@ -0,0 +1,13 @@
+import {
+  Component,
+} from "@angular/core";
+
+import { AuthFormComponent } from "./auth-form/auth-form.component";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "app.component.html"
+})
+export class AppComponent {
+  
+}
